c5d00569e1b380c0be80eae561bb56e01edb7c70
shunrunwangcong <1147431798@qq.com>
2025-08-19 18:20:47 +0800

    设置部署时刷新

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -2,6 +2,7 @@ window.PLAYER_CONFIG = {
   API_BASE_URL: 'https://api.sub-dog.top',
   SUBTITLE_NEED_LOGIN: true,
   ALLOW_PLAY_WITHOUT_LOGIN: true,
+  CAPTCHA_ENABLED: false,  // 禁用图形验证码
   CAPTCHA_SITE_KEY: '492e9548-748a-44ca-8c32-28961402fe44',
   I18N: {
     resend: '获取验证码',
